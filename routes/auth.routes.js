bfd325ead7b25ea52cf027463f5836b110273de0
Fikri-Ramadan <fikrinr.tech@gmail.com>
2023-07-26 09:58:46 +0700

    version 3

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,9 +4,6 @@ const authController = require("../controllers/auth.controller");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.redirect("/home");
-})
 
 router.get("/home", (req, res) => {
     res.render("dashboard");
